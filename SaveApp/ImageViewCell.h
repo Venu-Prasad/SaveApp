2686845aed5af15fbca914c6365467634379ab67
Apple <apple@Apples-iMac.local>
2019-03-12 15:59:00 +0530

    Third Commit

---
+++ SaveApp/ImageViewCell.h
@@ -0,0 +1,17 @@
+//
+//  ImageViewCell.h
+//  SaveApp
+//
+//  Created by Apple on 12/03/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ImageViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SaveApp/ImageViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ImageViewCell.m
+//  SaveApp
+//
+//  Created by Apple on 12/03/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+#import "ImageViewCell.h"
+
+@implementation ImageViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
